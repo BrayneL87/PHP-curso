b40bd0c75728b55bb76126aceaded1fcd859a726
87 <144358868+BrayneL87@users.noreply.github.com>
2023-10-02 18:50:02 +0000

    main

--- operadores-logicos.php
+++ operadores-logicos.php
@@ -24,3 +24,18 @@ $resultado = $michis_4_patas and $michis_programan_con_PHP;
 var_dump( $resultado );
 
 echo "\n";
+
+//suponiendo estas variables
+$es_un_michi_grande = true;
+$le_gusta_comer = true;
+$sabe_volar = false;
+$tiene_2_patas = false;
+// cual es el resultado?
+//$resultadoo = $es_un_michi_grande && $le_gusta_comer; //true
+//$resultadoo = $es_un_michi_grande || $sabe_volar; //True
+//$resultadoo = $sabe_volar || $tiene_2_patas; //false
+//$resultadoo = $sabe_volar || $tiene_2_patas; //false
+//$resultadoo = !$le_gusta_comer; // false
+$resultadoo = !$le_gusta_comer || $es_un_michi_grande; // true
+
+var_dump( $resultadoo);
