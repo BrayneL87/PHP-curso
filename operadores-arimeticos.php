14cc70aa14f9a4a22109a75db4521b287073e9bc
87 <144358868+BrayneL87@users.noreply.github.com>
2023-10-02 19:35:55 +0000

    main

--- operadores-arimeticos.php
+++ operadores-arimeticos.php
@@ -1,13 +1,21 @@
 <?php
-
-/* $resultado = 5 ** 3; // 5 x 5 x 5
-echo $resultado; */
-
-// echo (5 * 6) . " " . (80 / 4);
+/*
+$resultado_ = 5 + 3;
+$resultado = 5 ** 3; // 5 x 5 x 5
+echo $resultado_;
+echo "\n";
+echo $resultado; 
+echo "\n";
+*/
+/*
+echo (5 * 6) . " " . (80 / 4);
+echo "\n";
 
 /* echo 5 / 2 . "\n";
-echo 5 % 2; */
+echo 5 % 2;//Modulo 
+ */
 
+echo "7200 segundos convertidos a horas son " . 7200 / 3600 . " horas" . "\n";
 $horas = 7501;
 
 echo "$horas segundo convertidos a horas son " . ($horas / 3600) . "\n";
