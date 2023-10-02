5beccf4d64a7bde916db0ca335ac3cc78a93ef3e
87 <144358868+BrayneL87@users.noreply.github.com>
2023-10-02 21:21:57 +0000

    main

--- precedencia.php
+++ precedencia.php
@@ -1,21 +1,25 @@
 <?php
-
+/*
 $contador = 1;
 $resultado = $contador++;
 
 echo $resultado; 
-
-// Tiene asociatividad izquierda
-// echo 1 - 2 - 3 - 4;
-
+*/
+//---------------------------------------
+/*
+// Tiene asociatividad izquierda lo que se coloca entre parenteci se ejwcuta primero 
+ echo 1 - 2 - 3 - 4;
+*/
+//----------------------------------------
 // Tiene asociatividad derecha
 
-// $b = 5;
-/* $c = 10;
+ //$b = 5;
+ $c = 10;
 
 $a = $b = $c;
 
-echo $a; */
+echo $a;
+//------------------------------------------
 /*
 $michis_4_patas = true;
 $michis_programan_con_PHP = false;
