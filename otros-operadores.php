518ad470efe0e0f8d09153ad51bf127ffe18cb41
87 <144358868+BrayneL87@users.noreply.github.com>
2023-10-02 20:56:12 +0000

    main

--- otros-operadores.php
+++ otros-operadores.php
@@ -1,18 +1,23 @@
 <?php
 
 // Operador de asignación
-/* $edad_de_jaimito = ($edad_de_pepito = 18) + 5;
+/*
+$edad_de_jaimito = ($edad_de_pepito = 18) + 5;
 
 echo "La edad de pepito es $edad_de_pepito \n";
-echo "La edad de jaimito es $edad_de_jaimito"; */
-
+echo "La edad de jaimito es $edad_de_jaimito"; 
+*/
+//---------------------------------------------------
 // Operadores de incremento
 
 $contador = 1;
 
-// $contador = $contador + 1;
-// $contador += 1;
-// $contador++;
+//$contador = $contador + 1;
+//echo $contador;
+//$contador += 1; // forma mas corta de sumar la variable 
+//echo $contador;
+//$contador++;
+//echo $contador; // esta forma ++ solo se incrementa en 1
 
 /* 
 
@@ -20,13 +25,15 @@ $contador = 1;
 -= 
 *=
 /=
+.= concatenar
 
 */
-
-/* $nombre = "Carlos";
+/*
+$nombre = "Carlos";
 $nombre .=  " " . "Santana";
 
-echo $nombre; */
+echo $nombre;
+*/
 
 $contador = 1;
 $resultado = $contador++;
