203a2e9872d0bf2fbdcd2c6ce2e090a1c54ad807
87 <144358868+BrayneL87@users.noreply.github.com>
2023-10-03 16:35:53 +0000

    main

--- precedencia.php
+++ precedencia.php
@@ -12,20 +12,21 @@ echo $resultado;
 */
 //----------------------------------------
 // Tiene asociatividad derecha
-
+/*
  //$b = 5;
  $c = 10;
 
 $a = $b = $c;
 
 echo $a;
+*/
 //------------------------------------------
-/*
+
 $michis_4_patas = true;
 $michis_programan_con_PHP = false;
 
 $resultado = ($michis_4_patas and $michis_programan_con_PHP);
-
+// hay que encerrar entre parentecis para que escoja el resultado que se va mostrar 
 var_dump( $resultado );
-*/
+
 echo "\n";
